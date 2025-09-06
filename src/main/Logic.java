b17362eb6939533467535c3b609b1d9c433ca7ba
JemappeIIe <luka.chernyshov@outlook.com>
2025-09-06 04:52:53 +0200

    Initial commit

---
+++ src/main/Logic.java
@@ -0,0 +1,111 @@
+package main;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Logic {
+    public static final String ABSOLUTE_PROJECT_PATH = System.getProperty("user.dir");
+    public static final String IMAGE_PATH = ABSOLUTE_PROJECT_PATH + "/src/main/resources/images/";
+    public static final String ASCII_PATH = ABSOLUTE_PROJECT_PATH + "/src/main/resources/ascii/";
+    // https://stackoverflow.com/a/74186686
+    public static final char[] ASCII = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@".toCharArray();
+    public static final Map<Character, Integer> ASCII_CHAR_INDEX = new HashMap<>();
+
+    static {
+        for (int i = 0; i < ASCII.length; ++i) {
+            ASCII_CHAR_INDEX.put(ASCII[i], i);
+        }
+    }
+
+    UserInterface userInterface;
+
+    public Logic(UserInterface userInterface) {
+        this.userInterface = userInterface;
+    }
+
+    public File readASCIIFile(String filename) {
+        return new File(ASCII_PATH + filename);
+    }
+
+    public BufferedImage readImageFile(String filename) throws IOException {
+        File file = new File(IMAGE_PATH + filename);
+        BufferedImage image = ImageIO.read(file);
+        if (image == null) {
+            throw new IOException("(Error) File format is not supported");
+        }
+        return image;
+    }
+
+    public void writeASCII(BufferedImage image, String outputFileName) throws IOException {
+        File file = new File(ASCII_PATH + outputFileName);
+        PrintWriter printWriter = new PrintWriter(new FileWriter(file));
+        for (int y = 0; y < image.getHeight(); ++y) {
+            StringBuilder row = new StringBuilder();
+            for (int x = 0; x < image.getWidth(); ++x) {
+                int color = image.getRGB(x, y);
+                int red = (color & 0x00ff0000) >> 16;
+                int green = (color & 0x0000ff00) >> 8;
+                int blue = color & 0x000000ff;
+                double luminance = this.colorToLuminance(red, green, blue);
+                row.append(ASCII[luminanceToCharIndex(luminance)]);
+            }
+            printWriter.println(row);
+        }
+        printWriter.close();
+    }
+
+    public double colorToLuminance(int R, int G, int B) {
+        // https://stackoverflow.com/a/596243, Rec. 709
+        return (0.2126 * R + 0.7152 * G + 0.0722 * B);
+    }
+
+    public int luminanceToCharIndex(double Y) {
+        return (int) (ASCII.length * Y / 255);
+    }
+
+    public int charIndexToLuminance(int I) {
+        int Y = 255 / (ASCII.length - 1) * I;
+        int ARGB = 0xff000000;
+        // R << 16 | G << 8 | B
+        ARGB = (Y << 16) | (Y << 8) | Y;
+        return ARGB;
+    }
+
+    public void writeImage(String ASCIIFilename, String imageFilename, String imageFormat) {
+        File ascii = readASCIIFile(ASCIIFilename);
+        ArrayList<ArrayList<Integer>> ASCIIRowsConvertedToLuminance = new ArrayList<>();
+        int height = 0;
+        int width = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(ascii))) {
+            String row;
+            while ((row = bufferedReader.readLine()) != null) {
+                ASCIIRowsConvertedToLuminance.add(new ArrayList<>());
+                for (char character : row.toCharArray()) {
+                    ASCIIRowsConvertedToLuminance.get(height).add(charIndexToLuminance(Logic.ASCII_CHAR_INDEX.get(character)));
+                }
+                ++height;
+                if (width < row.length()) {
+                    width = row.length();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                image.setRGB(x, y, ASCIIRowsConvertedToLuminance.get(y).get(x));
+            }
+        }
+        File outputFile = new File(Logic.IMAGE_PATH + imageFilename);
+        try {
+            ImageIO.write(image, imageFormat, outputFile);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/UserInterface.java
@@ -0,0 +1,99 @@
+package main;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class UserInterface {
+    public static final String GET_IMAGE_FILENAME_MESSAGE = "Enter image filename: ";
+    public static final String GET_IMAGE_FORMAT_MESSAGE = "Enter image format: ";
+    public static final String GET_ASCII_FILENAME_MESSAGE = "Enter ASCII filename: ";
+    private static final Map<Integer, Runnable> commandMap = new HashMap<>();
+
+    static {
+        commandMap.put(0, UserInterface::stop);
+        commandMap.put(1, UserInterface::showASCII);
+        commandMap.put(2, UserInterface::convertToASCII);
+        commandMap.put(3, UserInterface::convertToImage);
+    }
+
+    private static Scanner scanner;
+    private static Logic logic;
+
+
+    public UserInterface(Scanner scanner) {
+        UserInterface.scanner = scanner;
+        logic = new Logic(this);
+    }
+
+    public void start() {
+        while (true) {
+            showCommands();
+            int command;
+            try {
+                command = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            commandMap.get(command).run();
+        }
+    }
+
+    public static String readInput(String message) {
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+
+    public void showCommands() {
+        System.out.println("(1) Show ASCII art");
+        System.out.println("(2) Convert image to ASCII art");
+        System.out.println("(3) Convert ASCII art to image");
+        System.out.println("(0) Stop program");
+        System.out.println("Select a command: ");
+    }
+
+    public static void showASCII() {
+        String filename = readInput(GET_ASCII_FILENAME_MESSAGE);
+        File file = logic.readASCIIFile(filename);
+        try (Scanner scannerASCII = new Scanner(file)) {
+            while (scannerASCII.hasNextLine()) {
+                System.out.println(scannerASCII.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void convertToASCII() {
+        String imageFilename = readInput(GET_IMAGE_FILENAME_MESSAGE);
+        BufferedImage image;
+        try {
+            image = logic.readImageFile(imageFilename);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        String ASCIIFilename = readInput(GET_ASCII_FILENAME_MESSAGE);
+        try {
+            logic.writeASCII(image, ASCIIFilename);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void convertToImage() {
+        String ASCIIFilename = readInput(GET_ASCII_FILENAME_MESSAGE);
+        String imageFilename = readInput(GET_IMAGE_FILENAME_MESSAGE);
+        String imageFormat = readInput(GET_IMAGE_FORMAT_MESSAGE);
+        logic.writeImage(ASCIIFilename, imageFilename, imageFormat);
+    }
+
+    public static void stop() {
+        System.exit(0);
+    }
+}
